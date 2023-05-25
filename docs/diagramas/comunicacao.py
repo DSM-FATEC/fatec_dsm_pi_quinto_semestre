2c15a58916c4b9459f58b3809a4b305c81083db3
Gustavo Henrique Pinto <ghp2201@gmail.com>
2023-05-25 19:57:02 -0300

    docs: ajusta diagramas

--- docs/diagramas/comunicacao.py
+++ docs/diagramas/comunicacao.py
@@ -5,7 +5,7 @@ import diagrams.onprem.queue as oq
 import diagrams.onprem.database as od
 
 
-with d.Diagram('Comunicação Entre Serviços'):
+with d.Diagram('', filename='comunicação_entre_serviços', graph_attr={'margin': '-1.5, -1.5'}):
     api = pf.FastAPI('API')
     bd = od.PostgreSQL('Banco do dados')
     artefato = pl.Python('Artefato')
--- docs/diagramas/infraestrutura.py
+++ docs/diagramas/infraestrutura.py
@@ -2,7 +2,7 @@ import diagrams as d
 import diagrams.aws.compute as ac
 import diagrams.onprem.queue as oq
 
-with d.Diagram('Infraestrutura'):
+with d.Diagram('', filename='infraestrutura', graph_attr={'margin': '-1.25, -1.25'}):
     with d.Cluster('AWS'):
         lightsail_api = ac.Lightsail('API')
         lightsail_db = ac.Lightsail('Banco de dados')
